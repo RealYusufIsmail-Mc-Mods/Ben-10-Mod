7994f33dbbd772f73c5ce22b0909af8daf2cbb1a
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-20 15:38:53 +0000

    update

--- src/main/java/com/Yusuf/ben10mobmod/core/init/iteminit.java
+++ src/main/java/com/Yusuf/ben10mobmod/core/init/iteminit.java
@@ -1,5 +1,10 @@
 package com.Yusuf.ben10mobmod.core.init;
 
-public class iteminit {
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
+public class iteminit {
+	
+	public static final DeferredRegister<Item> Items = DeferredRegister.create(ForgeRegistries.ITEMS, )
+  
 }
