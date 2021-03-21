c0266b06828479d8f57dcaa338d1a3ab866e5c6b
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-21 18:11:53 +0000

    Update Main.java

--- src/main/java/com/Yusuf/ben10mobmod/Main.java
+++ src/main/java/com/Yusuf/ben10mobmod/Main.java
@@ -1,16 +1,20 @@
 package com.Yusuf.ben10mobmod;
 
 import org.apache.logging.log4j.LogManager;
+
 import org.apache.logging.log4j.Logger;
 
+import com.Yusuf.ben10mobmod.core.init.BlockInit;
 import com.Yusuf.ben10mobmod.core.init.ItemInit;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("ben10mobmod")
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Bus.MOD)
 public class Main {
 	// Directly reference a log4j logger.
 	public static final Logger LOGGER = LogManager.getLogger();
@@ -20,6 +24,7 @@ public class Main {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		
 		ItemInit.ITEMS.register(bus);
+		BlockInit.BLOCKS.register(bus);
 		
 		MinecraftForge.EVENT_BUS.register(this);
 	}
