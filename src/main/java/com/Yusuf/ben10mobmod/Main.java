72fffc4767e7e353700a7f0d05eac5f83e8bb95e
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-21 18:44:54 +0000

    update

--- src/main/java/com/Yusuf/ben10mobmod/Main.java
+++ src/main/java/com/Yusuf/ben10mobmod/Main.java
@@ -1,14 +1,19 @@
 package com.Yusuf.ben10mobmod;
 
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.logging.log4j.Logger;
 
 import com.Yusuf.ben10mobmod.core.init.BlockInit;
 import com.Yusuf.ben10mobmod.core.init.ItemInit;
 
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -22,11 +27,21 @@ public class Main {
 
 	public Main() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		
+
 		ItemInit.ITEMS.register(bus);
 		BlockInit.BLOCKS.register(bus);
-		
+
 		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+		BlockInit.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block -> {
+			event.getRegistry().register(new BlockItem(block, new Item.Properties().group(ItemGroup.COMBAT))
+					.setRegistryName(block.getRegistryName()));
+
+		});
+
 	}
 
 }
