3f169caad432f251c2db4e53c099201635eb963e
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-22 11:20:52 +0000

    Update ItemInit.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
@@ -1,7 +1,7 @@
 package com.Yusuf.bentenmobmod.core.init;
 
 import com.Yusuf.bentenmobmod.Main;
-
+import com.Yusuf.bentenmobmod.core.itemgroup.MainItemGroup;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -14,7 +14,7 @@ public class ItemInit {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
 
 	public static final RegistryObject<Item> SWORD = ITEMS.register("sword",
-			() -> new Item(new Item.Properties().group(ItemGroup.COMBAT)));
+			() -> new Item(new Item.Properties().group(MainItemGroup.MAIN)));
 	
 	
 
