569f1cf2ee82ac6be2d99130a1952f8ecdd67de6
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-23 20:42:58 +0000

    Update ItemInit.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
@@ -17,9 +17,9 @@ public class ItemInit {
 
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
 	
-	public static final RegistryObject<Item> SWORD = ITEMS.register("sword", 
-			() -> new SwordItem(CustomToolMaterial.TOOL, 10, 1.6f, 
-					new Item.Properties().group(MainItemGroup.MAIN)));
+	public static final RegistryObject<SwordItem> SWORD = ITEMS.register("sword", 
+            () -> new SwordItem(CustomToolMaterial.TOOL, 10, 1.6f, 
+                    new Item.Properties().group(MainItemGroup.MAIN)));
 			
 
 	public static final RegistryObject<Item> LEGENDARY_ORE = ITEMS.register("legendary_ore",
