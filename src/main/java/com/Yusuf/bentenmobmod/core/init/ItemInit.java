5b48fe01e7d4a6ac79d8e8751dc92740809ad261
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-23 21:34:05 +0000

    update

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ItemInit.java
@@ -16,34 +16,31 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
 
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
-	
-	
-			
 
 	public static final RegistryObject<Item> LEGENDARY_ORE = ITEMS.register("legendary_ore",
 			() -> new Item(new Item.Properties().group(MainItemGroup.MAIN)));
-	
+
 	public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
 			() -> new Item(new Item.Properties().group(MainItemGroup.MAIN)));
-	
-	public static final RegistryObject<SwordItem> SWORD = ITEMS.register("sword", 
-            () -> new SwordItem(CustomToolMaterial.TOOL, 10, 1.6f, 
-                    new Item.Properties().group(MainItemGroup.MAIN)));
-	
-	public static final RegistryObject<Item> HELMET = ITEMS.register("helmet", 
-			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.HEAD, 
+
+	public static final RegistryObject<SwordItem> SWORD = ITEMS.register("sword",
+			() -> new SwordItem(CustomToolMaterial.TOOL, 10, 1.6f, 
+					new Item.Properties().group(MainItemGroup.MAIN)));
+
+	public static final RegistryObject<ArmorItem> HELMET = ITEMS.register("helmet",
+			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.HEAD,
 					new Item.Properties().group(MainItemGroup.MAIN)))));
-					
-	public static final RegistryObject<Item> CHESTPLATE = ITEMS.register("chestplate", 
-			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.CHEST, 
-					new Item.Properties().group(MainItemGroup.MAIN)))));		
-	
-	public static final RegistryObject<Item> BOOTS = ITEMS.register("boot", 
-		    () -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.FEET, 
-				    new Item.Properties().group(MainItemGroup.MAIN)))));	
-				    
-    public static final RegistryObject<Item> LEGGINGS = ITEMS.register("leggings", 
-			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.LEGS, 
-				    new Item.Properties().group(MainItemGroup.MAIN)))));				    
+
+	public static final RegistryObject<ArmorItem> CHESTPLATE = ITEMS.register("chestplate",
+			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.CHEST,
+					new Item.Properties().group(MainItemGroup.MAIN)))));
+
+	public static final RegistryObject<ArmorItem> BOOTS = ITEMS.register("boot",
+			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.FEET,
+					new Item.Properties().group(MainItemGroup.MAIN)))));
+
+	public static final RegistryObject<ArmorItem> LEGGINGS = ITEMS.register("leggings",
+			() -> new ArmorItem(CustomArmorMaterial.ARMOUR, EquipmentSlotType.LEGS,
+					new Item.Properties().group(MainItemGroup.MAIN)))));
 
 }
