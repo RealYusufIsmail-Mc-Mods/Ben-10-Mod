ac91bf444235e62ce2cf6b0ff0526827ce9d0273
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-03-26 14:28:11 +0000

    Update VilgaxEntity.java

--- src/main/java/com/Yusuf/bentenmobmod/common/entity/boss/VilgaxEntity.java
+++ src/main/java/com/Yusuf/bentenmobmod/common/entity/boss/VilgaxEntity.java
@@ -2,7 +2,11 @@ package com.Yusuf.bentenmobmod.common.entity.boss;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IChargeableMob;
+import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -12,9 +16,27 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 		   _interface = IChargeableMob.class
 		)
 public class VilgaxEntity extends MonsterEntity {
+	
 
-	protected VilgaxEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
-		super(type, worldIn);
-		// TODO Auto-generated constructor stub
-	}
-}
+public VilgaxEntity(EntityType<? extends WitherEntity> wither, World world) {
+    super(wither, world);
+    this.setHealth(this.getMaxHealth());
+    this.getNavigator().setCanSwim(true);
+    this.experienceValue = 100;
+};
+
+
+protected SoundEvent getAmbientSound() {
+    return SoundEvents.ENTITY_WITHER_AMBIENT;
+ };
+
+ protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+    return SoundEvents.ENTITY_WITHER_HURT;
+ }
+
+ protected SoundEvent getDeathSound() {
+    return SoundEvents.ENTITY_WITHER_DEATH;
+ }}
+
+
+
