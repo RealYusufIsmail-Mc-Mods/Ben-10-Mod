9aa58b762e31f69ce94232174994d54fff95b0de
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-04-01 22:35:32 +0100

    update

--- src/main/java/com/Yusuf/bentenmobmod/common/events/PlayerEvents.java
+++ src/main/java/com/Yusuf/bentenmobmod/common/events/PlayerEvents.java
@@ -1,17 +1,14 @@
 package com.Yusuf.bentenmobmod.common.events;
 
 import com.Yusuf.bentenmobmod.Main;
-import com.Yusuf.bentenmobmod.core.init.ItemInit;
 
-import net.minecraft.item.Item;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;	
 
 @EventBusSubscriber(modid = Main.MOD_ID, bus = Bus.FORGE)
 public  class  PlayerEvents {
 
-
+/*
 	@SubscribeEvent 
 	public static void swordHitPLayer(PlayerHitEvent event) {
 
@@ -25,5 +22,5 @@ public  class  PlayerEvents {
 			
 		
 	}
-	
+	*/
 }
