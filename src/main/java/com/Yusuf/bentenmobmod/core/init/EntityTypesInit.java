13b06a07770b0c0ab2903f4ba3206fc4a66303e5
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-04-04 21:28:04 +0100

    Update EntityTypesInit.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
@@ -1,24 +1,21 @@
 
 package com.Yusuf.bentenmobmod.core.init;
 
-import javax.swing.text.html.parser.Entity;
-
 import com.Yusuf.bentenmobmod.Main;
 import com.Yusuf.bentenmobmod.common.entities.VilgaxEntity;
-import com.Yusuf.bentenmobmod.objects.items.ModSpawnEggItem;
 
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.world.chunk.Chunk.CreateEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class EntityTypesInit {
+public abstract class EntityTypesInit {
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES,
 			Main.MOD_ID);
 
-	    
+	public abstract void createStandardEntityType(CreateEntityType VariableDeclaratorId);
 	    
 	    private static final EntityType<VilgaxEntity> vilgax = createStandardEntityType("vilgax", VilgaxEntity::new,
 				EntityClassification.CREATURE, 0.7f, 1.56f);
@@ -29,13 +26,9 @@ public class EntityTypesInit {
 		
 		
 		// register spawn eggs
-		public static final RegistryObject<ModSpawnEggItem> VILGAX_SPAWN_EGG = ItemInit.register("vilgax_spawn_egg",
-				() -> new SpawnEggItem(vilgax, 0x339BFF, 0x133B60, spawn_egg_props));
+	
 		
-		private static <T extends Entity> EntityType<T> createStandardEntityType(String entity_name,
-				EntityType.IFactory<T> factory, EntityClassification classification, float width, float height) {
-			return EntityType.Builder.create(factory, classification).size(width, height)
-					.build(Main.MOD_ID + ":" + entity_name);
-		}
+		
+		
 		
 }
