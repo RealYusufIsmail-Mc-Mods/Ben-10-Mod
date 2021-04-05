8181118f4431b18c4e638b3b14ca5b79472f2907
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-04-05 20:21:48 +0100

    update

--- src/main/java/com/Yusuf/bentenmobmod/client/entityrenderer/VilgaxRenderer.java
+++ src/main/java/com/Yusuf/bentenmobmod/client/entityrenderer/VilgaxRenderer.java
@@ -1,10 +1,4 @@
 package com.Yusuf.bentenmobmod.client.entityrenderer;
 
-import com.Yusuf.bentenmobmod.client.model.VilgaxModel;
-import com.Yusuf.bentenmobmod.common.entities.VilgaxEntity;
-
-import net.minecraft.client.renderer.entity.MobRenderer;
-
-public class VilgaxRenderer extends MobRenderer<VilgaxEntity, VilgaxModel<VilgaxEntity>>{
-
+public class VilgaxRenderer {
 }
