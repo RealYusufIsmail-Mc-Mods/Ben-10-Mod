41c9583b39257079fd0a8dfe9f11d9ab0ad8ae59
unknown <4ksdxd@gmail.com>
2021-04-09 12:53:34 -0700

    This should fix it

--- src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
@@ -7,6 +7,7 @@ import com.Yusuf.bentenmobmod.entity.VilgaxEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,10 +16,14 @@ public abstract class EntityTypesInit {
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES,
 			Main.MOD_ID);
 
-	
-
 	private static final EntityType<VilgaxEntity> vilgax = createStandardEntityType("vilgax", VilgaxEntity::new,
 			EntityClassification.CREATURE, 2f, 2f);
+
+	//This is where to register the actual attributes of the entities
+	@SuppressWarnings("deprecation")
+	public static void registerEntityAttributes() {
+		GlobalEntityTypeAttributes.put(EntityTypesInit.VILGAX_ENTITY.get(), VilgaxEntity.registerAttributes().build());
+	}
 
 	// registering the entities
 	public static final RegistryObject<EntityType<VilgaxEntity>> VILGAX_ENTITY = ENTITY_TYPES.register("vilgax",
