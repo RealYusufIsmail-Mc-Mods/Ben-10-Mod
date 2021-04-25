d20662acd46ff5647f7585861416b0f8098e6c62
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-04-25 21:14:04 +0100

    add advancments

--- src/main/java/com/Yusuf/bentenmobmod/core/init/FeatureInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/FeatureInit.java
@@ -1,39 +1,15 @@
 package com.Yusuf.bentenmobmod.core.init;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.template.RuleTest;
-import net.minecraft.world.gen.placement.Placement;
-import net.minecraft.world.gen.placement.TopSolidRangeConfig;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class FeatureInit {
-	
-	public static void addOres(final BiomeLoadingEvent event) {
-		addOre(event, OreFeatureConfig.FillerBlockType.NATURAL_STONE , 
-				BlockInit.LEGENDARY_ORE_BLOCK.get().defaultBlockState(), 3, 1, 20, 3);
-		
-			addOre(event, OreFeatureConfig.FillerBlockType.NATURAL_STONE , 
-					BlockInit.RUBY_ORE.get().defaultBlockState(), 3, 1, 20, 3);
+	public void setup(final FMLCommonSetupEvent event)
+	{
+		event.enqueueWork(() -> {
+			//Moved the methods bellow into enqueue to make sure they dont cause issues during registration - andrew
+			ConfiguredFeatures.registerConfiguredFeatures();
 			
-			addOre(event, OreFeatureConfig.FillerBlockType.NATURAL_STONE , 
-					BlockInit.OMNITRIX_ORE.get().defaultBlockState(), 3, 1, 15, 3);
-			
-			addOre(event, OreFeatureConfig.FillerBlockType.NATURAL_STONE , 
-					BlockInit.SPEED_ORE.get().defaultBlockState(), 3, 1, 15, 3);
-			
-			addOre(event, OreFeatureConfig.FillerBlockType.NATURAL_STONE , 
-					BlockInit.FIRE_ORE.get().defaultBlockState(), 3, 1, 11, 2);
+		});
 	}
-	
-	public static void  addOre(final BiomeLoadingEvent event, RuleTest rule, BlockState state, int veinSize, int minHeight, int maxHeight, int amount) {
-		event.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
-				Feature.ORE.configured(new OreFeatureConfig(rule, state, veinSize))
-				.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
-				.squared().count(amount));
-	}
-
 }
 	
