19926a81daea65cadfc01833581cfa19567dc5cd
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-04-25 14:09:51 +0100

    Update HeatblastArmor.java

--- src/main/java/com/Yusuf/bentenmobmod/common/events/HeatblastArmor.java
+++ src/main/java/com/Yusuf/bentenmobmod/common/events/HeatblastArmor.java
@@ -19,25 +19,31 @@ public class HeatblastArmor extends ArmorItem implements IForgeItem {
 	}
 	@Override
 	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		
+		ItemStack boots = player.getItemBySlot(EquipmentSlotType.FEET);
+		ItemStack legs = player.getItemBySlot(EquipmentSlotType.LEGS);
+		ItemStack chest = player.getItemBySlot(EquipmentSlotType.CHEST);
+		ItemStack head = player.getItemBySlot(EquipmentSlotType.HEAD);
+		
 		EffectInstance effect = new EffectInstance(Effects.FIRE_RESISTANCE, 10, 0, false, false, true);
-		if (stack.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && stack.getItem() == ItemInit.HEATBLAST_BOOT.get() && stack.getItem() == ItemInit.HEATBLAST_HELMET.get() && stack.getItem() == ItemInit.HEATBLAST_LEGGINGS.get()) {
+		if (boots.getItem() == ItemInit.HEATBLAST_BOOT.get() && legs.getItem() == ItemInit.HEATBLAST_LEGGINGS.get() && chest.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && head.getItem() == ItemInit.HEATBLAST_HELMET.get()) {
 			player.addEffect(effect);
 		}
 		EffectInstance effect1 = new EffectInstance(Effects.REGENERATION, 10, 0, false, false, true);
-		if (stack.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && stack.getItem() == ItemInit.HEATBLAST_BOOT.get() && stack.getItem() == ItemInit.HEATBLAST_HELMET.get() && stack.getItem() == ItemInit.HEATBLAST_LEGGINGS.get()) {
+		if (boots.getItem() == ItemInit.HEATBLAST_BOOT.get() && legs.getItem() == ItemInit.HEATBLAST_LEGGINGS.get() && chest.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && head.getItem() == ItemInit.HEATBLAST_HELMET.get()) {
 			player.addEffect(effect1);
 		}
 		EffectInstance effect11 = new EffectInstance(Effects.ABSORPTION, 10, 2, false, false, true);
-		if (stack.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && stack.getItem() == ItemInit.HEATBLAST_BOOT.get() && stack.getItem() == ItemInit.HEATBLAST_HELMET.get() && stack.getItem() == ItemInit.HEATBLAST_LEGGINGS.get()) {
+		if (boots.getItem() == ItemInit.HEATBLAST_BOOT.get() && legs.getItem() == ItemInit.HEATBLAST_LEGGINGS.get() && chest.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && head.getItem() == ItemInit.HEATBLAST_HELMET.get()) {
 			player.addEffect(effect11);
 		}
 		EffectInstance effect111 = new EffectInstance(Effects.DAMAGE_RESISTANCE, 10, 0, false, false, true);
-		if (stack.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && stack.getItem() == ItemInit.HEATBLAST_BOOT.get() && stack.getItem() == ItemInit.HEATBLAST_HELMET.get() && stack.getItem() == ItemInit.HEATBLAST_LEGGINGS.get()) {
+		if (boots.getItem() == ItemInit.HEATBLAST_BOOT.get() && legs.getItem() == ItemInit.HEATBLAST_LEGGINGS.get() && chest.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && head.getItem() == ItemInit.HEATBLAST_HELMET.get()) {
 			player.addEffect(effect111);
 			
 			}
 		EffectInstance effect12 = new EffectInstance(Effects.DAMAGE_BOOST, 10, 1, false, false, true);
-		if (stack.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && stack.getItem() == ItemInit.HEATBLAST_BOOT.get() && stack.getItem() == ItemInit.HEATBLAST_HELMET.get() && stack.getItem() == ItemInit.HEATBLAST_LEGGINGS.get()) {
+		if (boots.getItem() == ItemInit.HEATBLAST_BOOT.get() && legs.getItem() == ItemInit.HEATBLAST_LEGGINGS.get() && chest.getItem() == ItemInit.HEATBLAST_CHESTPLATE.get() && head.getItem() == ItemInit.HEATBLAST_HELMET.get()) {
 			player.addEffect(effect12);
 		}
 		
