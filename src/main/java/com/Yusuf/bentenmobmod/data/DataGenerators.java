2fe75288e8271d9f498b303c606822c09b59f458
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-05-07 14:06:50 +0100

    Create DataGenerators.java

---
+++ src/main/java/com/Yusuf/bentenmobmod/data/DataGenerators.java
@@ -0,0 +1,10 @@
+package com.Yusuf.bentenmobmod.data;
+
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+public class DataGenerators {
+
+    public static void gatherData(GatherDataEvent event) {
+        
+    }
+}
