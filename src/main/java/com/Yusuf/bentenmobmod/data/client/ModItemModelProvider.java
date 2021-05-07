33fd4ee12b0cc750ceefb009109200d1b41d65c6
Yusuf Ismail <67903886+realyusufismail@users.noreply.github.com>
2021-05-07 16:35:23 +0100

    Update ModItemModelProvider.java

--- src/main/java/com/Yusuf/bentenmobmod/data/client/ModItemModelProvider.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/client/ModItemModelProvider.java
@@ -1,6 +1,24 @@
 package com.Yusuf.bentenmobmod.data.client;
 
-public class ModItemModelProvider {
+import com.Yusuf.bentenmobmod.Main;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.data.ExistingFileHelper;
 
-    
+public class ModItemModelProvider extends ItemModelProvider {
+
+
+    public ModItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, Main.MOD_ID, existingFileHelper);
+    }
+
+
+
+    @Override
+    protected void registerModels() {
+        withExistingParent("heatblast_sword", modLoc("item/heatblast_sword"));
+
+        ModelFile itemGenerated = getExistingFile(mcLoc("item/generated"));
+    }
 }
