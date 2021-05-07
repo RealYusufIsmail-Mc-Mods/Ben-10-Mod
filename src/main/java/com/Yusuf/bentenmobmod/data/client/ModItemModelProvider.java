defaf98e43df94a123648d72a54f85f1806b149a
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-07 19:03:03 +0100

    Update ModItemModelProvider.java

--- src/main/java/com/Yusuf/bentenmobmod/data/client/ModItemModelProvider.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/client/ModItemModelProvider.java
@@ -2,6 +2,7 @@ package com.Yusuf.bentenmobmod.data.client;
 
 import com.Yusuf.bentenmobmod.Main;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -19,8 +20,13 @@ public class ModItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
         withExistingParent("black_diamond_block", modLoc("block/black_diamond_block"));
         withExistingParent("black_diamond_ore", modLoc("block/black_diamond_ore"));
-        withExistingParent("black_diamond", modLoc("item/black_diamond"));
 
         ModelFile itemGenerated = getExistingFile(mcLoc("item/generated"));
+
+        builder(itemGenerated, "black_diamond");
+    }
+
+    private ItemModelBuilder builder(ModelFile itemGenerated, String name) {
+       return getBuilder(name).parent(itemGenerated).texture("layer0", "item/" + name);
     }
 }
