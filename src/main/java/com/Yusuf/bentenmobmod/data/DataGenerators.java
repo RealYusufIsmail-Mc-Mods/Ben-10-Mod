e5fc448c958671f68a10ae8eb371fe7c501f1351
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-08 12:51:26 +0100

    Update DataGenerators.java

--- src/main/java/com/Yusuf/bentenmobmod/data/DataGenerators.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/DataGenerators.java
@@ -1,6 +1,9 @@
 package com.Yusuf.bentenmobmod.data;
 
 import com.Yusuf.bentenmobmod.Main;
+import com.Yusuf.bentenmobmod.data.client.ModItemModelProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -12,6 +15,10 @@ public final class DataGenerators {
 
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
-        ;
+        DataGenerator gen = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        gen.addProvider(new ModItemModelProvider(gen, existingFileHelper));
+
     }
 }
