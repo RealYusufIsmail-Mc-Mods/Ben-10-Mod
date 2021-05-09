ed10f7d3b1c6a63d810c98bc5eb1b136376fff1d
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-09 16:50:39 +0100

    Update ModTags.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ModTags.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ModTags.java
@@ -11,6 +11,7 @@ import net.minecraft.util.ResourceLocation;
 public class ModTags {
 
     public static final class Blocks {
+        ///ores
         public static final ITag.INamedTag<Block> ORES_BLACK_DIAMOND = forge("ores/black_diamond");
         public static final ITag.INamedTag<Block> ORES_RUBY = forge("ores/ruby");
         public static final ITag.INamedTag<Block> ORES_INFINITUM = forge("ores/infinitum");
@@ -18,7 +19,14 @@ public class ModTags {
         public static final ITag.INamedTag<Block> ORES_SPEED = forge("ores/speed");
         public static final ITag.INamedTag<Block> ORES_OMNITRIX = forge("ores/omnitrix");
 
-
+        //blocks
+        public static final ITag.INamedTag<Block> STORAGE_LEGENDARY = forge("storage_blocks/legendary");
+        public static final ITag.INamedTag<Block> STORAGE_OMNITRIX = forge("storage_blocks/omnitrix");
+        public static final ITag.INamedTag<Block> STORAGE_INFINITUM = forge("storage_blocks/infinitum");
+        public static final ITag.INamedTag<Block> STORAGE_FIRE = forge("storage_blocks/fire");
+        public static final ITag.INamedTag<Block> STORAGE_SPEED = forge("storage_blocks/speed");
+        public static final ITag.INamedTag<Block> STORAGE_BLACK_DIAMOND = forge("storage_blocks/black_diamond");
+        public static final ITag.INamedTag<Block> STORAGE_VILGAX = forge("storage_blocks/vilgax");
 
         private static ITag.INamedTag<Block> forge(String path) {
             return BlockTags.bind(new ResourceLocation("forge", path).toString());
@@ -30,13 +38,23 @@ public class ModTags {
     }
 
     public static final class Items {
+        //ores
         public static final ITag.INamedTag<Item> ORES_BLACK_DIAMOND = forge("ores/black_diamond");
         public static final ITag.INamedTag<Item> ORES_RUBY = forge("ores/ruby");
         public static final ITag.INamedTag<Item> ORES_INFINITUM = forge("ores/infinitum");
         public static final ITag.INamedTag<Item> ORES_FIRE = forge("ores/fire");
         public static final ITag.INamedTag<Item> ORES_SPEED = forge("ores/speed");
         public static final ITag.INamedTag<Item> ORES_OMNITRIX = forge("ores/omnitrix");
+        //blocks
+        public static final ITag.INamedTag<Item> STORAGE_LEGENDARY = forge("storage_blocks/legendary");
+        public static final ITag.INamedTag<Item> STORAGE_OMNITRIX = forge("storage_blocks/omnitrix");
+        public static final ITag.INamedTag<Item> STORAGE_INFINITUM = forge("storage_blocks/infinitum");
+        public static final ITag.INamedTag<Item> STORAGE_FIRE = forge("storage_blocks/fire");
+        public static final ITag.INamedTag<Item> STORAGE_SPEED = forge("storage_blocks/speed");
+        public static final ITag.INamedTag<Item> STORAGE_BLACK_DIAMOND = forge("storage_blocks/black_diamond");
+        public static final ITag.INamedTag<Item> STORAGE_VILGAX = forge("storage_blocks/vilgax");
 
+        //items
         public static final ITag.INamedTag<Item> INGOTS_LEGENDARY_ORE = forge("ingots/legendary_ore");
         public static final ITag.INamedTag<Item> INGOTS_OMNITRIX = forge("ingots/omnitrix");
         public static final ITag.INamedTag<Item> INGOTS_FIRE = forge("ingots/fire");
@@ -46,6 +64,9 @@ public class ModTags {
         public static final ITag.INamedTag<Item> INGOTS_BLACK_DIAMOND_SCRAP = forge("ingots/black_diamond_scrap");
         public static final ITag.INamedTag<Item> INGOTS_INFINITUM = forge("ingots/infinitum");
         public static final ITag.INamedTag<Item> INGOTS_INFINITUM_SCRAP = forge("ingots/infinitum_scrap");
+
+        //armour and wepons
+
 
         private static ITag.INamedTag<Item> forge(String path) {
             return ItemTags.bind(new ResourceLocation("forge", path).toString());
