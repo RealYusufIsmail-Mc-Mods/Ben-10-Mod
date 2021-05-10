63496233cd40c906125da7fdf5af9cf8e112ca2d
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-10 15:41:42 +0100

    update

--- src/main/java/com/Yusuf/bentenmobmod/data/ModRecipeProvider.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/ModRecipeProvider.java
@@ -3,21 +3,18 @@ package com.yusuf.bentenmobmod.data;
 import com.yusuf.bentenmobmod.core.init.BlockInit;
 import com.yusuf.bentenmobmod.core.init.ItemInit;
 import net.minecraft.data.*;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.function.Consumer;
 
-public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
-    public ModRecipeProvider(DataGenerator gen) {
-        super(gen);
+public class ModRecipeProvider extends RecipeProvider {
+    public ModRecipeProvider(DataGenerator generatorIn) {
+        super(generatorIn);
     }
 
-    @Override
-    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
-        protected void buildShapelessRecipes(Consumer < IFinishedRecipe > consumer)
-        {
+@Override
+    protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer) {
             ShapelessRecipeBuilder.shapeless(ItemInit.INFINITUM.get(), 9)
+
                     .requires(BlockInit.INFINITUM_BLOCK.get())
                     .unlockedBy("has_item", has(ItemInit.INFINITUM.get()))
                     .save(consumer);
@@ -30,11 +27,10 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                     .unlockedBy("has_item", has(ItemInit.INFINITUM.get()))
                     .save(consumer);
         }
-        private static ResourceLocation modId(String path){
-            return new ResourceLocation(Main.MOD_ID, path);
-        }
+
+
     }
-}
+
 
 
 
