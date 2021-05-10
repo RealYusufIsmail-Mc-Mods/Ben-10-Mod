c7f15d4de94c588cff54348f8ed20425392c6b80
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-10 17:41:21 +0100

    Update ModRecipeProvider.java

--- src/main/java/com/Yusuf/bentenmobmod/data/ModRecipeProvider.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/ModRecipeProvider.java
@@ -3,6 +3,8 @@ package com.yusuf.bentenmobmod.data;
 import com.yusuf.bentenmobmod.core.init.BlockInit;
 import com.yusuf.bentenmobmod.core.init.ItemInit;
 import net.minecraft.data.*;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
 
 import java.util.function.Consumer;
 
@@ -26,6 +28,18 @@ public class ModRecipeProvider extends RecipeProvider {
                     .pattern("###")
                     .unlockedBy("has_item", has(ItemInit.INFINITUM.get()))
                     .save(consumer);
+
+    SmithingRecipeBuilder.smithing(Ingredient.of(Items.DIAMOND_SWORD),Ingredient.of(ItemInit.BLACK_DIAMOND.get()),ItemInit.BLACK_DIAMOND_SWORD.get()).
+      unlocks("has_item", has(ItemInit.BLACK_DIAMOND_SWORD.get()));
+
+    ShapedRecipeBuilder.shaped(ItemInit.BLACK_DIAMOND_SWORD.get())
+            .define('#', ItemInit.INFINITUM.get())
+            .define('K', ItemInit.INFINITUM.get())
+            .pattern("###")
+            .pattern("###")
+            .pattern("###")
+            .unlockedBy("has_item", has(ItemInit.INFINITUM.get()))
+            .save(consumer);
 
         }
 
