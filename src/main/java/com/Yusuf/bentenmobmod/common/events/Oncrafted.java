341ccac9babf5da40a3c1b5b71b4cbcdb709ff00
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-12 23:55:55 +0100

    update

---
+++ src/main/java/com/Yusuf/bentenmobmod/common/events/Oncrafted.java
@@ -0,0 +1,25 @@
+/*
+package com.yusuf.bentenmobmod.common.events;
+
+import com.yusuf.bentenmobmod.core.init.ItemInit;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.SwordItem;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
+
+public class Oncrafted {
+
+    private Object Item;
+
+    @SubscribeEvent
+
+    public void craftingTableEvent(PlayerEvent.ItemCraftedEvent event) {
+        if Item = ItemInit.HEATBLAST_SWORD {
+            event.craftMatrix.addEnchantment(Enchantment.getEnchantmentByLocation("efficiency"), 2);
+
+        }
+    }
+
+}
+*/
