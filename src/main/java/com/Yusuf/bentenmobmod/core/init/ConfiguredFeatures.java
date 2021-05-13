8eb98c255d60699f74ea8ac6de4249aacde71244
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-13 07:27:44 +0100

    Update ConfiguredFeatures.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ConfiguredFeatures.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ConfiguredFeatures.java
@@ -1,18 +1,14 @@
 package com.yusuf.bentenmobmod.core.init;
 
 import com.yusuf.bentenmobmod.Main;
-import net.minecraft.block.BlockState;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
-import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.template.RuleTest;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.placement.TopSolidRangeConfig;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 public class ConfiguredFeatures {
 
@@ -53,14 +49,14 @@ public class ConfiguredFeatures {
 		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "black_diamond_ore"), BLACK_DIAMOND_ORE);
 		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "infinitum_ore"), INFINITUM_ORE);
 	}
-
+/*
 	public static void addOre(final BiomeLoadingEvent event, RuleTest rule, BlockState state, int veinSize,
 							  int minHeight, int maxHeight, int amount) {
 		event.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
 				Feature.ORE.configured(new OreFeatureConfig(rule, state, veinSize))
 						.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
 						.squared().count(amount));
-
+*/ //cuase game to crash. DOnt think is right
 	}
-}
+
 
