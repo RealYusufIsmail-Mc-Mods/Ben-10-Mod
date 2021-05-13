cce1ad7875a18308adb8ef62e1789de23ea53ebc
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-13 07:21:05 +0100

    Update ConfiguredFeatures.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/ConfiguredFeatures.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/ConfiguredFeatures.java
@@ -1,53 +1,66 @@
 package com.yusuf.bentenmobmod.core.init;
 
 import com.yusuf.bentenmobmod.Main;
-	import net.minecraft.util.ResourceLocation;
-	import net.minecraft.util.registry.Registry;
-	import net.minecraft.util.registry.WorldGenRegistries;
-	import net.minecraft.world.gen.feature.ConfiguredFeature;
-	import net.minecraft.world.gen.feature.Feature;
-	import net.minecraft.world.gen.feature.OreFeatureConfig;
-	import net.minecraft.world.gen.placement.Placement;
-	import net.minecraft.world.gen.placement.TopSolidRangeConfig;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.template.RuleTest;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 public class ConfiguredFeatures {
-	
-	
+
+
 	public static ConfiguredFeature<?, ?> LEGENDARY_ORE_BLOCK = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.LEGENDARY_ORE_BLOCK.get().defaultBlockState(), 3))
-            .decorated(Placement.RANGE.configured(new TopSolidRangeConfig(3, 1, 20)))
-            .squared().count(3);
-	
+			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(3, 1, 20)))
+			.squared().count(3);
+
 
 	public static ConfiguredFeature<?, ?> RUBY_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.RUBY_ORE.get().defaultBlockState(), 3))
-            .decorated(Placement.RANGE.configured(new TopSolidRangeConfig(3, 1, 20)))
-            .squared().count(3);
+			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(3, 1, 20)))
+			.squared().count(3);
 
 	public static ConfiguredFeature<?, ?> BLACK_DIAMOND_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.BLACK_DIAMOND_ORE.get().defaultBlockState(), 3))
 			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(3, 1, 20)))
 			.squared().count(3);
 	public static ConfiguredFeature<?, ?> SPEED_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.SPEED_ORE.get().defaultBlockState(), 3))
-            .decorated(Placement.RANGE.configured(new TopSolidRangeConfig(2, 1, 15)))
-            .squared().count(3);
+			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(2, 1, 15)))
+			.squared().count(3);
 	public static ConfiguredFeature<?, ?> OMNITRIX_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.OMNITRIX_ORE.get().defaultBlockState(), 3))
-            .decorated(Placement.RANGE.configured(new TopSolidRangeConfig(4, 1, 20)))
-            .squared().count(3);
+			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(4, 1, 20)))
+			.squared().count(3);
 	public static ConfiguredFeature<?, ?> FIRE_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHER_ORE_REPLACEABLES, BlockInit.FIRE_ORE.get().defaultBlockState(), 3))
-            .decorated(Placement.RANGE.configured(new TopSolidRangeConfig(4, 1, 20)))
-            .squared().count(3);
+			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(4, 1, 20)))
+			.squared().count(3);
 
 	public static ConfiguredFeature<?, ?> INFINITUM_ORE = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.INFINITUM_ORE.get().defaultBlockState(), 3))
 			.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(2, 1, 20)))
 			.squared().count(3);
+
 	public static void registerConfiguredFeatures() {
 		Registry<ConfiguredFeature<?, ?>> registry = WorldGenRegistries.CONFIGURED_FEATURE;
-	    Registry.register(registry, new ResourceLocation(Main.MOD_ID, "legendary_ore_block"), LEGENDARY_ORE_BLOCK);
-	    Registry.register(registry, new ResourceLocation(Main.MOD_ID, "ruby_ore"), RUBY_ORE);
-	    Registry.register(registry, new ResourceLocation(Main.MOD_ID, "speed_ore"), SPEED_ORE);
-	    Registry.register(registry, new ResourceLocation(Main.MOD_ID, "omnitrix_ore"), OMNITRIX_ORE);
-	    Registry.register(registry, new ResourceLocation(Main.MOD_ID, "fire_ore"), FIRE_ORE);
+		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "legendary_ore_block"), LEGENDARY_ORE_BLOCK);
+		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "ruby_ore"), RUBY_ORE);
+		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "speed_ore"), SPEED_ORE);
+		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "omnitrix_ore"), OMNITRIX_ORE);
+		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "fire_ore"), FIRE_ORE);
 		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "black_diamond_ore"), BLACK_DIAMOND_ORE);
 		Registry.register(registry, new ResourceLocation(Main.MOD_ID, "infinitum_ore"), INFINITUM_ORE);
 	}
 
+	public static void addOre(final BiomeLoadingEvent event, RuleTest rule, BlockState state, int veinSize,
+							  int minHeight, int maxHeight, int amount) {
+		event.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+				Feature.ORE.configured(new OreFeatureConfig(rule, state, veinSize))
+						.decorated(Placement.RANGE.configured(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
+						.squared().count(amount));
+
+	}
 }
 
