61b1fbdc476747a87bff3f144d8aa9bdfdb5ebdd
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-17 19:58:59 +0100

    Update ModBlockStateProvider.java

--- src/main/java/com/Yusuf/bentenmobmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/Yusuf/bentenmobmod/data/client/ModBlockStateProvider.java
@@ -16,8 +16,8 @@ public class ModBlockStateProvider extends BlockStateProvider{
     @Override
     protected void registerStatesAndModels() {
         simpleBlock(BlockInit.BLACK_DIAMOND_BLOCK.get());
-       simpleBlock(BlockInit.BLACK_DIAMOND_ORE.get());
-       simpleBlock(BlockInit.INFINITUM_ORE.get());
+        simpleBlock(BlockInit.BLACK_DIAMOND_ORE.get());
+        axisBlock((RotatedPillarBlock) BlockInit.INFINITUM_ORE.get(),modLoc("block/infinitum_ore_top"), modLoc("block/infinitum_ore_side"));
         simpleBlock(BlockInit.INFINITUM_BLOCK.get());
         simpleBlock(BlockInit.FIRE_BLOCK.get());
         simpleBlock(BlockInit.FIRE_ORE.get());
@@ -29,7 +29,7 @@ public class ModBlockStateProvider extends BlockStateProvider{
         simpleBlock(BlockInit.RUBY_ORE.get());
         simpleBlock(BlockInit.SPEED_BLOCK.get());
         simpleBlock(BlockInit.SPEED_ORE.get());
-        axisBlock((RotatedPillarBlock) BlockInit.IMPERIUM_ORE.get(), modLoc("block/infinitum_ore_top"), modLoc("block/infinitum_ore_side"));
+        simpleBlock(BlockInit.IMPERIUM_ORE.get());
         simpleBlock(BlockInit.VILGAX_BLOCK.get());
 
 
