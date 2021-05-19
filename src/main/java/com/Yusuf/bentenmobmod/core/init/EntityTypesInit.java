4f8cd75e9b84a7d7372c1d71ff25e90f526afad5
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-19 21:52:34 +0100

    Update EntityTypesInit.java

--- src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
+++ src/main/java/com/Yusuf/bentenmobmod/core/init/EntityTypesInit.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public abstract class EntityTypesInit {
+public class EntityTypesInit {
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES,
 			Main.MOD_ID);
 
