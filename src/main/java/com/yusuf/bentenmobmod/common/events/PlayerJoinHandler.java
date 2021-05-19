eddb8222120b20b0983319c37aacd5d7a1a1f973
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-19 22:14:42 +0100

    Update PlayerJoinHandler.java

--- src/main/java/com/yusuf/bentenmobmod/common/events/PlayerJoinHandler.java
+++ src/main/java/com/yusuf/bentenmobmod/common/events/PlayerJoinHandler.java
@@ -12,6 +12,26 @@ public class PlayerJoinHandler {
 
     @SubscribeEvent
     public static void playerLogin(PlayerEvent.PlayerLoggedInEvent evt) {
+
+        /* Advancements are not synced immediately on login, but on the player's first server tick.
+         * Thus, there is a short time clientside where the world is active and rendering but book contents
+         * are not loaded (because we load when advancements do).
+         * This can cause crashes for multiple reasons.
+         * Since this method is idempotent and the advancements are already loaded serverside, it should be safe
+         * to call this a bit earlier than vanilla does.
+         * Ideally, we want to make it so that any attempts to access the books cleanly receive dummy/blank data
+         * until the contents load, but alas here we are.
+         * TODO: Do that^
+         *//* Advancements are not synced immediately on login, but on the player's first server tick.
+         * Thus, there is a short time clientside where the world is active and rendering but book contents
+         * are not loaded (because we load when advancements do).
+         * This can cause crashes for multiple reasons.
+         * Since this method is idempotent and the advancements are already loaded serverside, it should be safe
+         * to call this a bit earlier than vanilla does.
+         * Ideally, we want to make it so that any attempts to access the books cleanly receive dummy/blank data
+         * until the contents load, but alas here we are.
+         * TODO: Do that^
+         */
         ((ServerPlayerEntity) evt.getPlayer()).getAdvancements().flushDirty((ServerPlayerEntity) evt.getPlayer());
 
     }
