d3bd3c5f4ccbef2844cf1a8b1fbf3fdf6373bcd4
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-22 17:09:31 +0100

    Update ModBlockLootTables.java

--- src/main/java/com/yusuf/bentenmobmod/data/loot/ModBlockLootTables.java
+++ src/main/java/com/yusuf/bentenmobmod/data/loot/ModBlockLootTables.java
@@ -1,7 +1,12 @@
 package com.yusuf.bentenmobmod.data.loot;
 
+import com.yusuf.bentenmobmod.Main;
 import com.yusuf.bentenmobmod.core.init.BlockInit;
+import net.minecraft.block.Block;
 import net.minecraft.data.loot.BlockLootTables;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.stream.Collectors;
 
 public class ModBlockLootTables extends BlockLootTables {
 
@@ -9,6 +14,11 @@ public class ModBlockLootTables extends BlockLootTables {
     protected void addTables() {
         dropSelf(BlockInit.OMNITRIX_BLOCK.get());
 }
-
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ForgeRegistries.BLOCKS.getValues().stream()
+                .filter(block -> Main.MOD_ID.equals(block.getRegistryName().getNamespace()))
+                .collect(Collectors.toSet());
+    }
     }
 
