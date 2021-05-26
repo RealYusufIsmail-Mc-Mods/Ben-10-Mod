a497ef30cfed28b79b42fc92e4624ab1d24b502e
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-26 08:49:22 +0100

    add all ores to generator

--- src/main/java/com/yusuf/bentenmobmod/core/world/gen/OreGeneration.java
+++ src/main/java/com/yusuf/bentenmobmod/core/world/gen/OreGeneration.java
@@ -11,11 +11,42 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class OreGeneration  {
     private static void setupOreGenerations() {
-    for(Biome biome : ForgeRegistries.BIOMES) {
-        biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                        BlockInit.BLACK_DIAMOND_ORE.get().getDefaultState(), 4)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(5, 3, 0, 16))));
-    }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+                            BlockInit.BLACK_DIAMOND_ORE.get().getDefaultState(), 4)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(3, 1, 0, 16))));
+        }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+                            BlockInit.LEGENDARY_ORE_BLOCK.get().getDefaultState(), 3)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 1, 0, 18))));
+        }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+                            BlockInit.IMPERIUM_ORE.get().getDefaultState(), 3)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 1, 0, 18))));
+        }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+                            BlockInit.SPEED_ORE.get().getDefaultState(), 3)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 1, 0, 18))));
+        }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+                            BlockInit.OMNITRIX_ORE.get().getDefaultState(), 5)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(5, 1, 0, 18))));
+        }
+        //nether
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                            BlockInit.INFINITUM_ORE.get().getDefaultState(), 3)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 1, 0, 18))));
+        }
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                            BlockInit.FIRE_ORE.get().getDefaultState(), 2)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 1, 0, 20))));
+        }
     }
 
 
