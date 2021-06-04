d597e589ce025191bc916233a8d04bf49a2451ea
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-04 20:52:19 +0100

    Update ModLootTables.java

--- src/main/java/com/yusuf/bentenmod/data/loot/ModLootTables.java
+++ src/main/java/com/yusuf/bentenmod/data/loot/ModLootTables.java
@@ -20,7 +20,7 @@ public class ModLootTables extends LootTableProvider {
 
     @Override
     public String getName() {
-        return "Ben Ten Mob Mod - Loot Tables";
+        return "Ben Ten Mod - Loot Tables";
     }
     @Override
     protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
