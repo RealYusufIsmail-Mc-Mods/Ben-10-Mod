97a258a902273f13a0cd3c9adbbe9e3302fb7a4b
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-07 09:05:27 +0100

    adding new sword

--- src/main/java/com/yusuf/bentenmod/core/init/ItemInit.java
+++ src/main/java/com/yusuf/bentenmod/core/init/ItemInit.java
@@ -108,6 +108,10 @@ public class ItemInit {
 			() -> new HoeItem(CustomToolMaterial.INFINITUM_HOE, 0, 7f,
 					new Item.Properties().stacksTo(1).durability(600).tab(MainItemGroup.MAIN)));
 
+	public static final RegistryObject<SwordItem> IMPERIUM_SWORD = ITEMS.register("imperium_sword",
+	 () ->  new SwordItem(CustomToolMaterial.IMPERIUM_SWORD, 0, 7f,
+			 new Item.Properties().stacksTo(1).durability(600).tab(MainItemGroup.MAIN)));
+
 //armour
 
 	// knight armour
