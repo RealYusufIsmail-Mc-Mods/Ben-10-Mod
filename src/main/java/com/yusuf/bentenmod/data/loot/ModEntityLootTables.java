11a60b8abc5e347c541469dfcc89c82aa5d92f62
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-09 11:23:38 +0100

    update

--- src/main/java/com/yusuf/bentenmod/data/loot/ModEntityLootTables.java
+++ src/main/java/com/yusuf/bentenmod/data/loot/ModEntityLootTables.java
@@ -1,6 +1,21 @@
 package com.yusuf.bentenmod.data.loot;
 
-public class ModEntityLootTables {
+import com.yusuf.bentenmod.core.init.EntityTypesInit;
+import com.yusuf.bentenmod.core.init.ItemInit;
+import net.minecraft.data.loot.EntityLootTables;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.*;
+import net.minecraft.loot.functions.LootingEnchantBonus;
+import net.minecraft.loot.functions.SetCount;
 
+public class ModEntityLootTables extends EntityLootTables {
+
+  @Override
+    protected void addTables() {
+      add(EntityTypesInit.VILGAX_ENTITY, LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantRange.exactly(1)).add(ItemLootEntry.lootTableItem(ItemInit.ENCHANTED_OMNITRIX_APPLE.get()).apply(SetCount.setCount(RandomValueRange.between(0.0F, 1.0F))).apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 1.0F))))));
+
+
+  }
 
 }
--- src/main/java/com/yusuf/bentenmod/data/loot/ModLootTables.java
+++ src/main/java/com/yusuf/bentenmod/data/loot/ModLootTables.java
@@ -25,7 +25,9 @@ public class ModLootTables extends LootTableProvider {
     @Override
     protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
         return ImmutableList.of(
-                Pair.of(ModBlockLootTables::new, LootParameterSets.BLOCK)
+                Pair.of(ModBlockLootTables::new, LootParameterSets.BLOCK),
+                Pair.of(ModEntityLootTables::new, LootParameterSets.ENTITY)
+
         );
     }
     @Override
