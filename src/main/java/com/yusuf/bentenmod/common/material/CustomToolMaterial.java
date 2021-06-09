6333cb92faed429cff228468a3baa76537da4d61
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-09 09:09:55 +0100

    adding items to the lang file

--- src/main/java/com/yusuf/bentenmod/common/material/CustomToolMaterial.java
+++ src/main/java/com/yusuf/bentenmod/common/material/CustomToolMaterial.java
@@ -22,7 +22,7 @@ public enum CustomToolMaterial implements IItemTier {
 	INFINITUM_HOE(20, 3000, 70f, 7f, 40, () -> Ingredient.of(ItemInit.INFINITUM.get())),
 	AXE(50, 2500, 80f, 8, 50, () -> Ingredient.of(ItemInit.RUBY.get())),
 	IMPERIUM_SWORD(3, 3000, 70f, 7.5f, 40, () -> Ingredient.of(ItemInit.IMPERIUM.get())),
-	IMPERIUM_PICKAXE(3, 2500	, 40f, 6f, 40, () -> Ingredient.of(ItemInit.IMPERIUM.get())),
+	IMPERIUM_PICKAXE(2, 2500	, 40f, 6f, 40, () -> Ingredient.of(ItemInit.IMPERIUM.get())),
 	IMPERIUM_PICKAXE_UPGRADED(3, 3000, 50f, 5f, 40, () -> Ingredient.of(ItemInit.IMPERIUM.get()));
 
 
