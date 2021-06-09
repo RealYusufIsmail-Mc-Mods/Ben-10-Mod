5c10ef16c1cbbec6e5cf981e5b4bc7be54ad36d3
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-09 12:12:58 +0100

    added lootable for entity

--- src/main/java/com/yusuf/bentenmod/data/loot/ModEntityLootTables.java
+++ src/main/java/com/yusuf/bentenmod/data/loot/ModEntityLootTables.java
@@ -1,21 +1,33 @@
 package com.yusuf.bentenmod.data.loot;
 
+import com.yusuf.bentenmod.Main;
 import com.yusuf.bentenmod.core.init.EntityTypesInit;
 import com.yusuf.bentenmod.core.init.ItemInit;
 import net.minecraft.data.loot.EntityLootTables;
 import net.minecraft.entity.EntityType;
-import net.minecraft.item.Items;
 import net.minecraft.loot.*;
 import net.minecraft.loot.functions.LootingEnchantBonus;
 import net.minecraft.loot.functions.SetCount;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class ModEntityLootTables extends EntityLootTables {
 
-  @Override
+    @Override
     protected void addTables() {
-      add(EntityTypesInit.VILGAX_ENTITY, LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantRange.exactly(1)).add(ItemLootEntry.lootTableItem(ItemInit.ENCHANTED_OMNITRIX_APPLE.get()).apply(SetCount.setCount(RandomValueRange.between(0.0F, 1.0F))).apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 1.0F))))));
+        add(EntityTypesInit.VILGAX_ENTITY.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantRange.exactly(1)).add(ItemLootEntry.lootTableItem(ItemInit.ENCHANTED_OMNITRIX_APPLE.get()).apply(SetCount.setCount(RandomValueRange.between(0.0F, 2.0F))).apply(LootingEnchantBonus.lootingMultiplier(RandomValueRange.between(0.0F, 2.0F))))));
+
+    }
+    @Override
+    protected Iterable<EntityType<?>> getKnownEntities()
+    {
+        return StreamSupport.stream(ForgeRegistries.ENTITIES.spliterator(), false)
+                .filter(entry -> entry.getRegistryName() != null && entry.getRegistryName().getNamespace().equals(Main.MOD_ID))
+                .collect(Collectors.toSet());
+    }
 
 
-  }
 
 }
