4aba86ea54ed52aaa171d694155cb0ae4e30b40a
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-11 15:48:31 +0100

    update

--- src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
@@ -31,6 +31,6 @@ public class ModBlockStateProvider extends BlockStateProvider{
         simpleBlock(BlockInit.IMPERIUM_ORE.get());
         simpleBlock(BlockInit.IMPERIUM_BLOCK.get());
 
-        horizontalBlock(BlockInit.IMPERIUM_BLOCK.get(),modLoc("block/infinitum_ore_side"), modLoc("block/infinitum_ore_top"),modLoc("block/infinitum_ore_top"));
+        horizontalBlock(BlockInit.TABLE_BLOCK.get(),modLoc("block/infinitum_ore_top"), modLoc("block/table_block_front"),modLoc("block/table_block_bottom"));
     }
 }
