cb77a9406291f7a2324c5dd0f1158fcd6ab70f2e
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-15 21:16:11 +0100

    Update ClientEvents.java

--- src/main/java/com/yusuf/bentenmod/common/events/ClientEvents.java
+++ src/main/java/com/yusuf/bentenmod/common/events/ClientEvents.java
@@ -1,11 +1,15 @@
 package com.yusuf.bentenmod.common.events;
 
+import com.yusuf.bentenmod.Main;
 import com.yusuf.bentenmod.core.init.ContainerInit;
 import com.yusuf.bentenmod.core.machine.bententable.TableScreen;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
 
     private ClientEvents(){}
