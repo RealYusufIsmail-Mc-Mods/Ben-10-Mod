fe42c44e11de2e41541b207497fb23a01a7d07e7
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-17 16:39:22 +0100

    Update RotatedPillar.java

--- src/main/java/com/yusuf/bentenmod/core/init/RotatedPillar.java
+++ src/main/java/com/yusuf/bentenmod/core/init/RotatedPillar.java
@@ -9,16 +9,19 @@ import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 
 public class RotatedPillar extends RotatedPillarBlock {
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
 
 
-
-    public RotatedPillar(Properties p_i48440_1_) {
-        super(p_i48440_1_);
-
+    public RotatedPillar(Properties p_i48339_1_) {
+        super(p_i48339_1_);
     }
 
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> p_206840_1_) {
+        p_206840_1_.add(FACING);
     }
 
+    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getHorizontalDirection().getOpposite());
+    }
 }
