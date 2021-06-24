c610f3da866dc040a8227197ab873bf93dbf515e
vemerion <vemerion@gmail.com>
2021-06-24 15:44:20 +0200

    Add AI goals

--- src/main/java/com/yusuf/bentenmod/entity/KraabEntity.java
+++ src/main/java/com/yusuf/bentenmod/entity/KraabEntity.java
@@ -4,7 +4,13 @@ import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 public class KraabEntity extends CreatureEntity {
@@ -17,5 +23,14 @@ public class KraabEntity extends CreatureEntity {
         return MonsterEntity.createMonsterAttributes().add(Attributes.FOLLOW_RANGE, 35)
                 .add(Attributes.MOVEMENT_SPEED, 0.25).add(Attributes.ATTACK_DAMAGE, 4).add(Attributes.MAX_HEALTH, 25);
     }
+    
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1, false));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomWalkingGoal(this, 1));
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+     }
 
 }
