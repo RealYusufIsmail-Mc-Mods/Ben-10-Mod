b38f1f88560fff4b687793ed8dba301bb8d80ff1
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-25 11:24:22 +0100

    test

--- src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
@@ -3,14 +3,15 @@ package com.yusuf.bentenmod.data.client;
 
 import com.yusuf.bentenmod.Main;
 import com.yusuf.bentenmod.core.init.BlockInit;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.FinishedVariantBlockState;
+import net.minecraft.data.TexturedModel;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.yusuf.realyusufismailcore.block.IBlockProvider;
 
 public class ModBlockStateProvider extends BlockStateProvider{
     public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -35,25 +36,23 @@ public class ModBlockStateProvider extends BlockStateProvider{
         simpleBlock(BlockInit.SPEED_ORE.get());
         simpleBlock(BlockInit.IMPERIUM_ORE.get());
         simpleBlock(BlockInit.IMPERIUM_BLOCK.get());
-
-        horizontalBlock(BlockInit.TABLE_BLOCK.get(),modLoc("block/table_block_side"), modLoc("block/table_block_front"),modLoc("block/table_block_bottom"));
+      //  createFurnace(BlockInit.TABLE_BLOCK, TexturedModel.ORIENTABLE_ONLY_TOP);
     }
+
+
 
     private ModelFile.ExistingModelFile getExistingModel(String blockName) {
         return models().getExistingFile(modLoc(blockName));
     }
-    private void orientableMachineBlock(IBlockProvider block, String name) {
-        ModelFile.ExistingModelFile offModel = getExistingModel(name);
-        ModelFile.ExistingModelFile onModel = getExistingModel(name + "_on");
-        getVariantBuilder(block.asBlock()).forAllStates(state -> {
-            Direction facing = state.get(BlockStateProperties.HORIZONTAL_FACING);
-            boolean lit = state.get(BlockStateProperties.LIT);
-            return ConfiguredModel.builder()
-                    .modelFile(lit ? onModel : offModel)
-                    .rotationY((int) facing.getOpposite().toYRot())
-                    .build();
-        });
+
+    public void createFurnace(Block p_239977_1_, TexturedModel.ISupplier p_239977_2_) {
+        ResourceLocation resourcelocation = p_239977_2_.create(p_239977_1_, this.modelOutput);
+        ResourceLocation resourcelocation1 = p_239977_2_.get(p_239977_1_).updateTextures((p_239963_1_) -> {
+        }).createWithSuffix(p_239977_1_, "_on", this.modelOutput);
+        this.blockStateOutput.accept(FinishedVariantBlockState.multiVariant(p_239977_1_).with(createBooleanModelDispatch(BlockStateProperties.LIT, resourcelocation1, resourcelocation)).with(createHorizontalFacingDispatch()));
     }
+
+
 }
 
 
