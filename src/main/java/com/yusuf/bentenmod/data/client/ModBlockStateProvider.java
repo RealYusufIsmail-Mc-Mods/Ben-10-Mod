5d9e709e2d589cae368e797363afaf9a1440bcad
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-25 09:10:53 +0100

    testing

--- src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
@@ -1,11 +1,16 @@
-/*
+
 package com.yusuf.bentenmod.data.client;
 
 import com.yusuf.bentenmod.Main;
 import com.yusuf.bentenmod.core.init.BlockInit;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.yusuf.realyusufismailcore.block.IBlockProvider;
 
 public class ModBlockStateProvider extends BlockStateProvider{
     public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -33,7 +38,22 @@ public class ModBlockStateProvider extends BlockStateProvider{
 
         horizontalBlock(BlockInit.TABLE_BLOCK.get(),modLoc("block/table_block_side"), modLoc("block/table_block_front"),modLoc("block/table_block_bottom"));
     }
+
+    private ModelFile.ExistingModelFile getExistingModel(String blockName) {
+        return models().getExistingFile(modLoc(blockName));
+    }
+    private void orientableMachineBlock(IBlockProvider block, String name) {
+        ModelFile.ExistingModelFile offModel = getExistingModel(name);
+        ModelFile.ExistingModelFile onModel = getExistingModel(name + "_on");
+        getVariantBuilder(block.asBlock()).forAllStates(state -> {
+            Direction facing = state.get(BlockStateProperties.HORIZONTAL_FACING);
+            boolean lit = state.get(BlockStateProperties.LIT);
+            return ConfiguredModel.builder()
+                    .modelFile(lit ? onModel : offModel)
+                    .rotationY((int) facing.getOpposite().toYRot())
+                    .build();
+        });
+    }
 }
 
 
- */
