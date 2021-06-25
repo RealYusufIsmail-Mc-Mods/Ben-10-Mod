d296c5b44b8b20899af3341ea803344d38d12889
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-25 18:18:58 +0100

    test

--- src/main/java/com/yusuf/bentenmod/core/util/slot/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.yusuf.bentenmod.core.util.slot;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
