7a87be32ee2387aac36de3de4a6d896a7571fe5f
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-29 17:41:51 +0100

    testing

--- src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
@@ -8,6 +8,8 @@ import net.minecraft.data.*;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
+import static net.yusuf.realyusufismailcore.data.client.ModelProvider.BlockModels.createFurnace;
+
 public class ModBlockStateProvider extends BlockStateProvider {
 
     public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -33,7 +35,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
             simpleBlock(BlockInit.SPEED_ORE.get());
             simpleBlock(BlockInit.IMPERIUM_BLOCK.get());
             simpleBlock(BlockInit.IMPERIUM_ORE.get());
-           //createFurnace(BlockInit.TABLE_BLOCK, TexturedModel.ORIENTABLE_ONLY_TOP);
+            createFurnace(BlockInit.TABLE_BLOCK, TexturedModel.ORIENTABLE_ONLY_TOP);
 
     }
 
