d0aecc48c5138ba5681242ca757e714fc6adc476
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-12 22:44:37 +0100

    Update ContainerInit.java

--- src/main/java/com/yusuf/bentenmod/core/init/ContainerInit.java
+++ src/main/java/com/yusuf/bentenmod/core/init/ContainerInit.java
@@ -1,8 +1,6 @@
 package com.yusuf.bentenmod.core.init;
 
-import com.yusuf.bentenmod.core.machine.alientable.AlienTableContainer;
 import com.yusuf.bentenmod.core.machine.bententable.TableContainer;
-import com.yusuf.bentenmod.core.machine.testtable.TestContainer;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.fml.RegistryObject;
@@ -14,7 +12,5 @@ import static com.yusuf.bentenmod.BenTenMod.MOD_ID;
 public class ContainerInit {
     public static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, MOD_ID);
     public static final RegistryObject<ContainerType<TableContainer>> TABLE_CONTAINER = CONTAINERS.register("table", () -> IForgeContainerType.create(TableContainer::new));
-    public static final RegistryObject<ContainerType<TestContainer>> TEST_CONTAINER = CONTAINERS.register("test", () -> IForgeContainerType.create(TestContainer::new));
-    public static final RegistryObject<ContainerType<AlienTableContainer>> ALIEN_TABLE_CONTAINER = CONTAINERS.register("alien_table", () -> IForgeContainerType.create(AlienTableContainer::new));
 
 }
