1c19fe0eadee7ce05b6199e09a0d946283f2de82
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-13 21:30:13 +0100

    Update AlienTableContainer.java

--- src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTableContainer.java
+++ src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTableContainer.java
@@ -3,6 +3,7 @@ package com.yusuf.bentenmod.core.machine.alientable;
 import com.yusuf.bentenmod.core.init.BlockInit;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.WorkbenchContainer;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
@@ -13,11 +14,21 @@ public class AlienTableContainer extends WorkbenchContainer {
 
 
 
-
-    public AlienTableContainer(int id, World world, BlockPos pos, PlayerInventory playerInventory, PlayerEntity playerEntity, IWorldPosCallable p_i50090_3_) {
-        super(id, world, pos, playerInventory, playerEntity);
-        this.access = p_i50090_3_;
+    public AlienTableContainer(int id, PlayerInventory playerInventory) {
+        this(id, playerInventory, IWorldPosCallable.NULL);
     }
+
+    public AlienTableContainer(int id, PlayerInventory playerInventory, IWorldPosCallable access) {
+        super(ContainerType.CRAFTING, id);
+        this.access = access;
+    }
+
+    /*
+    public AlienTableContainer(int id, World world, BlockPos pos, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+        super();
+    }
+
+     */
 
     @Override
     public boolean stillValid(PlayerEntity player) {
