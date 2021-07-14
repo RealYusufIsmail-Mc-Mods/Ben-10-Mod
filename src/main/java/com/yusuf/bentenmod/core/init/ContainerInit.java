f7f4b3d80a5d7f0067b30883bcf6177f9c92a810
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-14 22:27:31 +0100

    Update ContainerInit.java

--- src/main/java/com/yusuf/bentenmod/core/init/ContainerInit.java
+++ src/main/java/com/yusuf/bentenmod/core/init/ContainerInit.java
@@ -12,7 +12,7 @@ import static com.yusuf.bentenmod.BenTenMod.MOD_ID;
 
 public class ContainerInit {
     public static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, MOD_ID);
-    public static final RegistryObject<ContainerType<TableContainer>> TABLE_CONTAINER = CONTAINERS.register("table", () -> IForgeContainerType.create(TableContainer::new));\    public static final RegistryObject<ContainerType<TableContainer>> TABLE_CONTAINER = CONTAINERS.register("table", () -> IForgeContainerType.create(TableContainer::new));
+    public static final RegistryObject<ContainerType<TableContainer>> TABLE_CONTAINER = CONTAINERS.register("table", () -> IForgeContainerType.create(TableContainer::new));
     public static final RegistryObject<ContainerType<AlienTableContainer>> ALIEN_TABLE_CONTAINER = CONTAINERS.register("alien_table", () -> IForgeContainerType.create(AlienTableContainer::new));
 
 
