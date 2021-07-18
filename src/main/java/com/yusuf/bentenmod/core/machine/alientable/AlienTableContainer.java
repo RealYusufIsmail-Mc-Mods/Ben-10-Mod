bf207a2fd65bea793ba395e2bd65b4fbbe6cb722
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-18 22:43:40 +0100

    Update AlienTableContainer.java

--- src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTableContainer.java
+++ src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTableContainer.java
@@ -4,11 +4,20 @@ package com.yusuf.bentenmod.core.machine.alientable;
 import com.yusuf.bentenmod.core.init.BlockInit;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.CraftResultInventory;
+import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.WorkbenchContainer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ICraftingRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.network.play.server.SSetSlotPacket;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Optional;
 
 
 public class AlienTableContainer extends WorkbenchContainer {
@@ -26,13 +35,23 @@ public class AlienTableContainer extends WorkbenchContainer {
 
     }
 
-/*
- @Override
-    public boolean stillValid(PlayerEntity player) {
-        return stillValid(this.access, player, BlockInit.ALIEN_TABLE.get());
-    }
- */
+    @Override
+    protected static void slotChangedCraftingGrid(int p_217066_0_, World p_217066_1_, PlayerEntity p_217066_2_, CraftingInventory p_217066_3_, CraftResultInventory p_217066_4_) {
+        if (!p_217066_1_.isClientSide) {
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)p_217066_2_;
+            ItemStack itemstack = ItemStack.EMPTY;
+            Optional<ICraftingRecipe> optional = p_217066_1_.getServer().getRecipeManager().getRecipeFor(IRecipeType.CRAFTING, p_217066_3_, p_217066_1_);
+            if (optional.isPresent()) {
+                ICraftingRecipe icraftingrecipe = optional.get();
+                if (p_217066_4_.setRecipeUsed(p_217066_1_, serverplayerentity, icraftingrecipe)) {
+                    itemstack = icraftingrecipe.assemble(p_217066_3_);
+                }
+            }
 
+            p_217066_4_.setItem(0, itemstack);
+            serverplayerentity.connection.send(new SSetSlotPacket(p_217066_0_, 0, itemstack));
+        }
+    }
 
 }
 
