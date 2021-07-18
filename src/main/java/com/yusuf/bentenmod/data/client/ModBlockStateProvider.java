70c4159b303ea5d803a6da092b4a893e2c1a1769
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-18 17:11:04 +0100

    update

--- src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
+++ src/main/java/com/yusuf/bentenmod/data/client/ModBlockStateProvider.java
@@ -3,7 +3,10 @@ package com.yusuf.bentenmod.data.client;
 import com.yusuf.bentenmod.BenTenMod;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nonnull;
@@ -50,9 +53,34 @@ public class ModBlockStateProvider extends BlockStateProvider {
         simpleBlock(DEEPSLATE_OMNITRIX_ORE.get());
         simpleBlock(DEEPSLATE_LEGENDARY_ORE.get());
         // orientableMachineBlock(TABLE_BLOCK.get(), "table_block");
+        tableBlock();
+        tableBlockOn();
+    }
+    @Override
+    public void horizontalBlock(Block block, ModelFile model) {
+        super.horizontalBlock(block, model);
+        this.simpleBlockItem(block, model);
+    }
+    public void tableBlock() {
+        ResourceLocation name = TABLE_BLOCK.get().getRegistryName();
+        assert name != null;
+        BlockModelBuilder builder = this.models().withExistingParent(name.getPath(), "block/orientable");
+        builder.texture("top", modLoc("block/table_block_top"));
+        builder.texture("front", modLoc("block/table_block_front"));
+        builder.texture("side", modLoc("block/table_block_side"));
+        this.simpleBlockItem(TABLE_BLOCK.get(), builder);
+    }
+    public void tableBlockOn() {
+        String name = "table_block_on";
+        assert name != null;
+        BlockModelBuilder builder = this.models().withExistingParent(name, "block/orientable");
+        builder.texture("top", modLoc("block/table_block_top"));
+        builder.texture("front", modLoc("block/table_block_front"));
+        builder.texture("side", modLoc("block/table_block_side"));
+
+        this.simpleBlockItem(TABLE_BLOCK.get(), builder);
     }
 }
-
 
 
 
