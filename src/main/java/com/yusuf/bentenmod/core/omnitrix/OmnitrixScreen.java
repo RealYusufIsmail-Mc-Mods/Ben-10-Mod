218057f7c6651378e7a1afa47342e2c9e1fdaf55
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-23 19:03:11 +0100

    Create OmnitrixScreen.java

---
+++ src/main/java/com/yusuf/bentenmod/core/omnitrix/OmnitrixScreen.java
@@ -0,0 +1,10 @@
+package com.yusuf.bentenmod.core.omnitrix;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.text.ITextComponent;
+
+public class OmnitrixScreen extends Screen {
+    protected OmnitrixScreen(ITextComponent p_i51108_1_) {
+        super(p_i51108_1_);
+    }
+}
