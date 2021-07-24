074a0db7e350e055feb256318de42b349835f4b3
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-24 16:48:18 +0100

    Update OmnitrixScreen.java

--- src/main/java/com/yusuf/bentenmod/core/omnitrix/OmnitrixScreen.java
+++ src/main/java/com/yusuf/bentenmod/core/omnitrix/OmnitrixScreen.java
@@ -1,10 +1,14 @@
 package com.yusuf.bentenmod.core.omnitrix;
 
+import com.yusuf.bentenmod.BenTenMod;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 public class OmnitrixScreen extends Screen {
     protected OmnitrixScreen(ITextComponent p_i51108_1_) {
         super(p_i51108_1_);
     }
+    public static final ResourceLocation SCREEN_ID = new ResourceLocation(BenTenMod.MOD_ID, "textures/gui/omnitrix.png");
+
 }
