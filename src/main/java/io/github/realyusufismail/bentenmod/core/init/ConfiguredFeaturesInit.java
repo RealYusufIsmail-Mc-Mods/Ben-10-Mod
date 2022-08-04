945e74cfae669f3a7d62fda46e6c393804bc0c6d
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-08-04 08:54:39 +0100

    spotless

--- src/main/java/io/github/realyusufismail/bentenmod/core/init/ConfiguredFeaturesInit.java
+++ src/main/java/io/github/realyusufismail/bentenmod/core/init/ConfiguredFeaturesInit.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.*;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
 
@@ -19,33 +20,33 @@ public class ConfiguredFeaturesInit {
     public static RegistryObject<?> register(String name, Holder<ConfiguredFeature<?, ?>> holder,
             int orePlacement, int aboveBottomInMinus, int aboveBottomInPositive, boolean rare) {
         if (!rare) {
-            return PLACED_FEATURES.register(name,
-                    () -> new PlacedFeature(holder,
-                            commonOrePlacement(orePlacement,
-                                    HeightRangePlacement.triangle(
-                                            VerticalAnchor.aboveBottom(aboveBottomInMinus),
-                                            VerticalAnchor.aboveBottom(aboveBottomInPositive)))));
+            return PLACED_FEATURES
+                .register(name,
+                        () -> new PlacedFeature(holder,
+                                commonOrePlacement(orePlacement, HeightRangePlacement.triangle(
+                                        VerticalAnchor.aboveBottom(aboveBottomInMinus),
+                                        VerticalAnchor.aboveBottom(aboveBottomInPositive)))));
         } else {
-            return PLACED_FEATURES.register(name,
-                    () -> new PlacedFeature(holder,
-                            rareOrePlacement(orePlacement,
-                                    HeightRangePlacement.triangle(
-                                            VerticalAnchor.aboveBottom(aboveBottomInMinus),
-                                            VerticalAnchor.aboveBottom(aboveBottomInPositive)))));
+            return PLACED_FEATURES
+                .register(name,
+                        () -> new PlacedFeature(holder,
+                                rareOrePlacement(orePlacement, HeightRangePlacement.triangle(
+                                        VerticalAnchor.aboveBottom(aboveBottomInMinus),
+                                        VerticalAnchor.aboveBottom(aboveBottomInPositive)))));
         }
     }
 
-    public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_,
+    public static @Unmodifiable List<PlacementModifier> orePlacement(PlacementModifier p_195347_,
             PlacementModifier p_195348_) {
         return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
     }
 
-    public static List<PlacementModifier> commonOrePlacement(int p_195344_,
+    public static @Unmodifiable List<PlacementModifier> commonOrePlacement(int p_195344_,
             PlacementModifier p_195345_) {
         return orePlacement(CountPlacement.of(p_195344_), p_195345_);
     }
 
-    public static List<PlacementModifier> rareOrePlacement(int p_195350_,
+    public static @Unmodifiable List<PlacementModifier> rareOrePlacement(int p_195350_,
             PlacementModifier p_195351_) {
         return orePlacement(RarityFilter.onAverageOnceEvery(p_195350_), p_195351_);
     }
