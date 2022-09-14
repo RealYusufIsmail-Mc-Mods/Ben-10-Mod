5baeb68797ebd8a7aa118ba6233f3ba355a970ec
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-09-14 17:33:26 +0100

    updated forge, minecraft and geaoklib version

--- src/main/java/io/github/realyusufismail/bentenmod/core/init/ServerGamePacketListenerInit.java
+++
@@ -1,9 +0,0 @@
-package io.github.realyusufismail.bentenmod.core.init;
-
-import io.github.realyusufismail.bentenmod.core.init.recipe.book.BenTenServerboundRecipeBookChangeSettingsPacket;
-import net.minecraft.network.protocol.game.ServerGamePacketListener;
-
-public interface ServerGamePacketListenerInit extends ServerGamePacketListener {
-    void handleCustomRecipeBookChangeSettingsPacket(
-            BenTenServerboundRecipeBookChangeSettingsPacket pPacket);
-}
