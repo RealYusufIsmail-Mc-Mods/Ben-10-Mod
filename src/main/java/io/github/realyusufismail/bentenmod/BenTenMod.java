a943c22e1bc39fc92a623c122424962c751f7f6b
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-12-20 06:36:46 +0000

    still having issues

--- src/main/java/io/github/realyusufismail/bentenmod/BenTenMod.java
+++ src/main/java/io/github/realyusufismail/bentenmod/BenTenMod.java
@@ -18,19 +18,30 @@
  */ 
 package io.github.realyusufismail.bentenmod;
 
+import io.github.realyusufismail.bentenmod.client.renderer.KraabRenderer;
+import io.github.realyusufismail.bentenmod.client.renderer.VilgaxRenderer;
 import io.github.realyusufismail.bentenmod.core.init.*;
+import io.github.realyusufismail.bentenmod.core.keybind.Keybindings;
 import io.github.realyusufismail.bentenmod.core.util.ModResourceLocation;
 import io.github.realyusufismail.bentenmod.data.DataGenerators;
+import io.github.realyusufismail.bentenmod.entity.KraabBoltEntity;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 import software.bernie.example.GeckoLibMod;
 import software.bernie.geckolib3.GeckoLib;
 
@@ -47,11 +58,12 @@ public class BenTenMod {
         TileEntityInit.TILE_ENTITY.register(bus);
         ContainerInit.CONTAINERS.register(bus);
 
-        GeckoLibMod.DISABLE_IN_DEV = true;
         GeckoLib.initialize();
 
         bus.addGenericListener(IRecipeSerializer.class, RegisterRecipeInit::registerRecipes);
         bus.addListener(DataGenerators::gatherData);
+        bus.addListener(this::onClientSetup);
+
         MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, OreGenerationInit::addOres);
         MinecraftForge.EVENT_BUS.addListener(this::onBiomeLoad);
         MinecraftForge.EVENT_BUS.register(this);
@@ -66,5 +78,20 @@ public class BenTenMod {
 
     public void onBiomeLoad(BiomeLoadingEvent event) {
         EntitySpawingInit.onBiomesLoad(event);
+    }
+
+    public void onClientSetup(final FMLClientSetupEvent event) {
+        Keybindings.RELOAD = new KeyBinding(
+                "key." + BenTenMod.MOD_ID + ".reload", GLFW.GLFW_KEY_R, "key.categories." + BenTenMod.MOD_ID);
+        ClientRegistry.registerKeyBinding(Keybindings.RELOAD);
+        RenderingRegistry.registerEntityRenderingHandler(EntityTypesInit.VILGAX_ENTITY.get(), VilgaxRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityTypesInit.KRAAB_ENTITY.get(), KraabRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(
+                EntityTypesInit.KRAAB_BOLT_ENTITY.get(), m -> new EntityRenderer<KraabBoltEntity>(m) {
+                    @Override
+                    public ResourceLocation getTextureLocation(KraabBoltEntity entity) {
+                        return null;
+                    }
+                });
     }
 }
